215a222d0e739e39e8cea53d6ade59503f7b0930
root <root@rancher.peter.lan4>
2019-07-10 09:30:56 +0800

    Fixed alembic staging upgrade

--- cronscript.sh
+++ cronscript.sh
@@ -7,7 +7,8 @@ export PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 cd /opt/authentication-server
 cat ./cronscript.sh
 pytest
-alembic upgrade head
+#alembic upgrade head
+alembic -c staging_alembic.ini upgrade head
 pserve staging.ini
 
 
