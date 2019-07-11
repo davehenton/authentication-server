ac5bc65e8188a26073b40dd82b0658f02acf9902
root <root@rancher.peter.lan4>
2019-07-11 09:48:09 +0800

    Removed debug lines in CI/CD init scripts

--- cicd.sh
+++ cicd.sh
@@ -5,10 +5,10 @@
 export PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
 cd /opt/authentication-server
-cat ./cronscript.sh
+#cat ./cronscript.sh
 pytest
 #alembic upgrade head
 alembic -c staging_alembic.ini upgrade head
-pserve staging.ini & 
+#pserve staging.ini & 
 
 
