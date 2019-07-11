2873e56bdf0aed5c4ff9e8ea73f81aefb31b2ab0
root <root@rancher.peter.lan4>
2019-07-11 10:30:36 +0800

    Adding Automated Code Coverage Initial testing automation logic3

--- cicd.sh
+++ cicd.sh
@@ -30,7 +30,8 @@ coverage xml
 
 echo "Pete engine ENV Generation"
 export GIT_COMMIT=$(git log | grep -m1 -oE '[^ ]+$')
-export CODECLIMATE_REPO_TOKEN='30423619d0d8265bce3b093504c08d9618be22da7c4264a821087c3ad5e0fc8a'
+#export CODECLIMATE_REPO_TOKEN='30423619d0d8265bce3b093504c08d9618be22da7c4264a821087c3ad5e0fc8a'
+export CC_TEST_REPORTER_ID='30423619d0d8265bce3b093504c08d9618be22da7c4264a821087c3ad5e0fc8a'
 export GIT_COMMITTED_AT="$(date +%s)"
 export GIT_BRANCH="$BRANCH"
 ./cc-test-reporter before-build 
